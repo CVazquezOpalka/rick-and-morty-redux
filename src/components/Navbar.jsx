ed30f23c3b60d3b7c74c272a65a448f6c89cdf06
CVazquezOpalka <cristianvazquez80@gmail.com>
2023-05-10 15:48:43 -0300

    primer nav terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,20 +13,20 @@ export const Navbar = () => {
   ];
   return (
     <Container>
-     <div className="topbar">
-     <picture>
-        <img src={HeroTitle} alt="logo de rick and morty" />
-      </picture>
-      <nav className="navigation">
-        <ul>
-          {links.map(({ to, title }) => (
-            <li key={title}>
-              <Link to={to}>{title}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-     </div>
+      <div className="topbar">
+        <picture>
+          <img src={HeroTitle} alt="logo de rick and morty" />
+        </picture>
+        <nav className="navigation">
+          <ul>
+            {links.map(({ to, title }) => (
+              <li key={title}>
+                <Link to={to}>{title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
       <div className="buttonbar">
         <Searchbar />
       </div>
@@ -35,56 +35,63 @@ export const Navbar = () => {
 };
 
 const Container = styled.header`
-width: 100%;
-height: 150px;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-.topbar{
+  width: 100vw;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .topbar {
     width: 100%;
-    height: 80px;
+    height: 110px;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    picture{
-        width: 300px;
-        height: 80px;
+    picture {
+      width: 300px;
+      height: 80px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 100%;
+        cursor: pointer;
+      }
+    }
+    .navigation {
+      width: 600px;
+      height: auto;
+      ul {
         display: flex;
         align-items: center;
-        justify-content: center;
-        img{
-            width: 100%;
-            cursor: pointer;
-        }
-    }
-    .navigation{
-        width: 600px;
-        height: auto;
-        ul{
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            gap: 10px;
-            list-style: none;
-            margin-right: 30px;
-            li{
-                font-size: 1.1rem;
-                font-weight: 500;
-                letter-spacing: 1px;
-                a{
-                    display: block;
-                    text-decoration: none;
-                    color: #fff;
-                    transition: 0.5s;
-                    &:hover{
-                        padding: 5px 10px;
-                        border: 1px solid #fff;
-                        border-radius: 10px;
-                    }
-                }
+        justify-content: space-between;
+        gap: 10px;
+        list-style: none;
+        margin-right: 30px;
+        li {
+          font-size: 1.1rem;
+          font-weight: 500;
+          letter-spacing: 1px;
+          a {
+            display: block;
+            text-decoration: none;
+            color: rgb(32, 233, 5);
+            transition: 0.5s;
+            &:hover {
+              padding: 5px 10px;
+              border: 1px solid rgb(32, 233, 5);
+              border-radius: 10px;
             }
+          }
         }
+      }
     }
-}
+  }
+  .buttonbar {
+    margin: 10px 0;
+    border: 1px solid rgb(32, 233, 5);
+    width: 1000px;
+    height: 40px;
+    border-radius: 35px;
+  }
 `;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const Searchbar = () => {
   return (
     <Container>
-      <h1>CSearchbar</h1>
+      <input type="text" />
+      <button>
+        <AiOutlineSearch />
+      </button>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 40px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  input{
+    border:none;
+    outline: none;
+    background: transparent;
+    width: 80%;
+    height: 100%;
+    font-size: 1.2rem;
+    color: rgb(32, 233, 5);
+  }
+  button{
+    width: 10%;
+    height:100%;
+    background: none;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    svg{
+      font-size: 30px;
+      color: rgb(32, 233, 5);
+    }
+  }
+`;
